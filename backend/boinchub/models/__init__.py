4018ed82bf94b0f6aa0d6da606e7cfc5a3d9aea7
Jason Lynch <jason@aexoden.com>
2025-06-09 21:19:00 -0700

    Switch to using timezone-aware timestamp fields in the database.
    While I am no longer using SQLite for development, previous tests
    indicated these didn't actually become timezone aware. However, it does
    work on PostgreSQL, which is the recommended database.
    
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- backend/boinchub/models/__init__.py
+++ backend/boinchub/models/__init__.py
@@ -5,18 +5,20 @@
 
 import datetime
 
-from sqlalchemy import func
-from sqlmodel import Field
+from sqlmodel import DateTime, Field, func
 
 
 class Timestamps:
     """Mixin for timestamp fields."""
 
     created_at: datetime.datetime = Field(
-        sa_column_kwargs={"server_default": func.now()}, default_factory=lambda: datetime.datetime.now(tz=datetime.UTC)
+        sa_type=DateTime(timezone=True),  # type: ignore[arg-type]
+        sa_column_kwargs={"server_default": func.now()},
+        default_factory=lambda: datetime.datetime.now(tz=datetime.UTC),
     )
 
     updated_at: datetime.datetime = Field(
+        sa_type=DateTime(timezone=True),  # type: ignore[arg-type]
         sa_column_kwargs={"server_default": func.now(), "onupdate": func.now()},
         default_factory=lambda: datetime.datetime.now(tz=datetime.UTC),
     )
